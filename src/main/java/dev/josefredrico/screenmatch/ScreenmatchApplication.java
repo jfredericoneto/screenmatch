9d284cea8087952e8824b681099573da944a22b0
Neto <jfredericon@gmail.com>
2024-01-20 12:53:18 -0300

    Criado o menu de interação com o usuário

--- src/main/java/dev/josefredrico/screenmatch/ScreenmatchApplication.java
+++ src/main/java/dev/josefredrico/screenmatch/ScreenmatchApplication.java
@@ -4,9 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import dev.josefredrico.screenmatch.model.DadosSerie;
-import dev.josefredrico.screenmatch.service.ConsumoApi;
-import dev.josefredrico.screenmatch.service.ConverteDados;
+import dev.josefredrico.screenmatch.principal.Principal;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner{
@@ -17,12 +15,8 @@ public class ScreenmatchApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
-		System.out.println(json);
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
 
 }
