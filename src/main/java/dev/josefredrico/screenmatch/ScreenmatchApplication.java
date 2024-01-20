d95212d236a8d9abf8e58ca03423446ef238d902
Neto <jfredericon@gmail.com>
2024-01-20 12:29:50 -0300

    Adicionado serviço para consumo da api

--- src/main/java/dev/josefredrico/screenmatch/ScreenmatchApplication.java
+++ src/main/java/dev/josefredrico/screenmatch/ScreenmatchApplication.java
@@ -1,13 +1,25 @@
 package dev.josefredrico.screenmatch;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import dev.josefredrico.screenmatch.model.DadosSerie;
+import dev.josefredrico.screenmatch.service.ConsumoApi;
+import dev.josefredrico.screenmatch.service.ConverteDados;
+
 @SpringBootApplication
-public class ScreenmatchApplication {
+public class ScreenmatchApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
+		System.out.println(json);
 	}
 
 }
